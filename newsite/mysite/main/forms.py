71172835ab251ce40b3c361cc2f59628f6e5c390
Joshua Dano <jgd5nm@virginia.edu>
2021-01-18 11:58:29 -0500

    experimenting with python forms

--- newsite/mysite/main/forms.py
+++ newsite/mysite/main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class invasive(forms.Form):
-	name = forms.CharField(label="Name", max_length=200)
-	check = forms.BooleanField()
+	age = forms.IntegerField()
+	ejf = forms.IntegerField()
