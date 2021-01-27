c01ed7e9cdb072647292d7129e804f2bdc3dc1f9
Joshua Dano <jgd5nm@virginia.edu>
2021-01-27 11:31:06 -0500

    added new path

--- newsite/mysite/main/urls.py
+++ newsite/mysite/main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.index, name="index"),
-path("all/", views.all, name="all"),
-path("form/", views.form, name="form"),
+	path("", views.index, name="index"),
+	path("all/", views.all, name="all"),
+	path("results/", views.results, name="results"),
+	path("external", views.external, name="script"),
 ]
