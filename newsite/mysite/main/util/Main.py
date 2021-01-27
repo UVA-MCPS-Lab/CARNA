5d2186c1a356f56ca2b422d6d2b4d133de6f2495
Joshua Dano <jgd5nm@virginia.edu>
2021-01-27 15:01:49 -0500

    moved scripts to util folder

---
+++ newsite/mysite/main/util/Main.py
@@ -0,0 +1,139 @@
+'''
+HemoPheno4HF
+SCRIPT DESCRIPTION: Main Runner Class for Learning Scores from the Trained MVDDs
+CODE DEVELOPED BY: Josephine Lamp, Steven Lamp
+ORGANIZATION: University of Virginia, Charlottesville, VA
+LAST UPDATED: 8/24/2020
+'''
+
+from . import MVDD
+from . import MVDD_Generator as gen
+# from MVDDGenerator
+import pandas as pd
+import networkx as nx
+from networkx.drawing.nx_pydot import *
+# import Params as params
+from . import Params as params
+
+
+#Expects param dict of 27 parameters, and select one of 4 outcomes
+#Returns a text file location to display the graph, a integer score value and a string phenotype to be displayed
+#Outcome can be "ALL", "DEATH" "REHOSPITALIZATION" "READMISSION" (passed in all caps)
+def runHemo(paramDict, outcome):
+
+    #check for strings in paramDict
+    for p in paramDict:
+        if paramDict[p] == "":
+            paramDict[p] = 0
+        else:
+            paramDict[p] = float(paramDict[p])
+
+    #Convert input into dataframe
+    input = pd.Series(paramDict)
+    input = input.to_frame()
+    input = input.T
+
+    #get outcome
+    if outcome == "READMISSION":
+        modelName = 'util/TreeFiles/Hemo_Readmission'
+    elif outcome == "DEATH":
+        modelName = 'main/util/TreeFiles/Hemo_Death'
+    elif outcome == "REHOSPITALIZATION":
+        modelName = 'util/TreeFiles/Hemo_Rehosp'
+    else:
+        modelName = 'util/TreeFiles/Hemo_AllOutcomes'
+
+    #load model
+    mvdd = gen.loadMVDDFromFile(modelName)
+
+    #Predict score
+    score, path = mvdd.predictScore(input)
+
+    if score == 5:
+        stringPath = "Returned Score of " + str(score) + ", Risk Level: HIGH\nIndicates a >= 40% chance of the outcome " + outcome
+    elif score == 4:
+        stringPath = "Returned Score of " + str(score) + ", Risk Level: INTERMEDIATE - HIGH\nIndicates a 30-40% chance of the outcome " + outcome
+    elif score == 3:
+        stringPath = "Returned Score of " + str(score) + ", Risk Level: INTERMEDIATE\nIndicates a 20-30% chance of the outcome " + outcome
+    elif score == 2:
+        stringPath = "Returned Score of " + str(score) + ", Risk Level: LOW - INTERMEDIATE\nIndicates a 10-20% chance of the outcome " + outcome
+    elif score == 1:
+        stringPath = "Returned Score of " + str(score) + ", Risk Level: LOW\nIndicates a < 10% chance of the outcome " + outcome
+
+
+    stringPath = ""
+    if path != None:
+        # path[-2] = '->'
+        stringPath = ' and '.join(path)
+        # print(stringPath)
+
+    imageName = modelName + '.png'
+    # imageName = "hi"
+    # score = 2
+    # stringPath = "no.jpg"
+
+    return imageName, score, stringPath #will be displayed on webpage
+
+
+#Expects a param dict of 119 parameters and the specified outcome
+#Returns a text file location to display the graph, a integer score value and a string phenotype to be displayed
+#Outcome can be "ALL", "DEATH" "REHOSPITALIZATION" "READMISSION" (passed in all caps)
+def runAllData(paramDict, outcome):
+    # check for strings in paramDict
+    for p in paramDict:
+        if paramDict[p] == "":
+            paramDict[p] = 0
+        else:
+            paramDict[p] = float(paramDict[p])
+
+    # Convert input into dataframe
+    input = pd.Series(paramDict)
+    input = input.to_frame()
+    input = input.T
+
+    # get outcome
+    if outcome == "READMISSION":
+        modelName = 'TreeFiles/AllData_Readmission'
+    elif outcome == "DEATH":
+        modelName = 'TreeFiles/AllData_Death'
+    elif outcome == "REHOSPITALIZATION":
+        modelName = 'TreeFiles/AllData_Rehosp'
+    else:
+        modelName = 'TreeFiles/AllData_AllOutcomes'
+
+    # load model
+    mvdd = gen.loadMVDDFromFile(modelName)
+
+    # Predict score
+    score, path = mvdd.predictScore(input)
+
+    if score == 5:
+        stringPath = "Returned Score of " + str(
+            score) + ", Risk Level: HIGH\nIndicates a >= 40% chance of the outcome " + outcome
+    elif score == 4:
+        stringPath = "Returned Score of " + str(
+            score) + ", Risk Level: INTERMEDIATE - HIGH\nIndicates a 30-40% chance of the outcome " + outcome
+    elif score == 3:
+        stringPath = "Returned Score of " + str(
+            score) + ", Risk Level: INTERMEDIATE\nIndicates a 20-30% chance of the outcome " + outcome
+    elif score == 2:
+        stringPath = "Returned Score of " + str(
+            score) + ", Risk Level: LOW - INTERMEDIATE\nIndicates a 10-20% chance of the outcome " + outcome
+    elif score == 1:
+        stringPath = "Returned Score of " + str(
+            score) + ", Risk Level: LOW\nIndicates a < 10% chance of the outcome " + outcome
+
+
+def main():
+    paramDict = {"Age": "60", "BPDIAS": "63", "BPSYS": "80", "CI": "2.02", "CO": "4.52", "CPI": "10", "PCWP": "10", "EjF": "20", "HRTRT": "30",
+     "MAP": "", "MIXED": "", "MPAP": "20", "PAD": "", "PAMN": "", "PAPP": "", "PAS": "11", "PCWPA": "32", "PCWPMN": "90",
+     "PCWPMod": "", "PP": "", "PPP": "", "PPRatio": "0.9", "RAP": "", "RAT": "", "RATHemo": "10", "SVRHemo": "",
+     "SVR": ""}
+
+    filename, score, path = runHemo(paramDict, "REHOSPITALIZATION")
+
+
+
+
+if __name__ == "__main__":
+    main()
--- newsite/mysite/main/util/calculate.py
+++ newsite/mysite/main/util/calculate.py
@@ -1,6 +1,6 @@
 # from flask import Flask, redirect, url_for, render_template, request, Response
 # app = Flask (__name__)
-from Main import *
+from .Main import *
 
 # @app.route('/',methods=["POST","GET"])
 # def renderMain():
@@ -48,50 +48,51 @@ from Main import *
 
 #     return render_template("main/base.html")
 
-def get_results():
-    # hemoDict = {'RAP': [0.0, 85.0],
-    #         'PAS': [0.0, 90.0],
-    #         'PAD': [0.0, 59.0],
-    #         'PAMN': [0.0, 82.0],
-    #         'PCWP': [0.0, 53.0],
-    #         'PCWPMod': [0.0, 53.0],
-    #         'PCWPA': [0.0, 53.0],
-    #         'PCWPMN': [0.0, 49.0],
-    #         'CO': [0.0, 25.0],
-    #         'CI': [0.0, 4.81],
-    #         'SVRHemo': [0.0, 6866.0],
-    #         'MIXED': [0.0, 627.0],
-    #         'BPSYS': [0.0, 168.0],
-    #         'BPDIAS': [0.0, 125.0],
-    #         'HRTRT': [0.0, 125.0],
-    #         'RATHemo': [0.0, 3.74],
-    #         'MAP': [0.0, 226.0],
-    #         'MPAP': [0.0, 129.3333333],
-    #         'CPI': [0.0, 1.820192166],
-    #         'PP': [-55.0, 106.0],
-    #         'PPP': [-0.833333333, 0.736842105],
-    #         'PAPP': [0.0, 0.8444444440000001],
-    #         'SVR': [0.0, 10755.555559999999],
-    #         'RAT': [0.0, 3.736842105],
-    #         'PPRatio': [-0.8870967740000001, 9.666666667000001],
-    #         'Age': [23.0, 88.0],
-    #         'EjF': [0.0, 45.0]}
+def get_results(data):
+    hemoDict = {'RAP': [0.0, 85.0],
+            'PAS': [0.0, 90.0],
+            'PAD': [0.0, 59.0],
+            'PAMN': [0.0, 82.0],
+            'PCWP': [0.0, 53.0],
+            # 'PCWPMod': [0.0, 53.0],
+            # 'PCWPA': [0.0, 53.0],
+            'PCWPMN': [0.0, 49.0],
+            'CO': [0.0, 25.0],
+            'CI': [0.0, 4.81],
+            # 'SVRHemo': [0.0, 6866.0],
+            'MIXED': [0.0, 627.0],
+            'BPSYS': [0.0, 168.0],
+            'BPDIAS': [0.0, 125.0],
+            # 'HRTRT': [0.0, 125.0],
+            # 'RATHemo': [0.0, 3.74],
+            # 'MAP': [0.0, 226.0],
+            'MPAP': [0.0, 129.3333333],
+            'CPI': [0.0, 1.820192166],
+            'PP': [-55.0, 106.0],
+            'PPP': [-0.833333333, 0.736842105],
+            'PAPP': [0.0, 0.8444444440000001],
+            'SVR': [0.0, 10755.555559999999],
+            'RAT': [0.0, 3.736842105],
+            'PPRatio': [-0.8870967740000001, 9.666666667000001],
+            'Age': [23.0, 88.0],
+            'EjF': [0.0, 45.0]}
         
-    # for key in hemoDict:
-    #     hemoDict[key] = request.form[key.lower()]
+    for key in hemoDict:
+        hemoDict[key] = data[key.lower()]
 
-    # # results = runHemo(hemoDict, params['testparam'])
-    # results = runHemo(hemoDict,request.form["testparam"])
-    # stringy = results[0]
-    # score = results[1]
-    # path = results[2]
-    # print(stringy, score, path)
+    # results = runHemo(hemoDict, params['testparam'])
+    results = runHemo(hemoDict,data["testparam"])
+    stringy = results[0]
+    score = results[1]
+    path = results[2]
+    print(stringy, score, path)
 
 
     # return render_template("main/results.html",imgsrc=("."+stringy[8:]),score=results[1],path=results[2])
-#         return results
+    #     return results
 
-    return request.form['pas']
+    # return request.form['pas']
+    return stringy, score, path
 
 
 # if __name__ == "__main__":
