4782c149261eb775314d49ec3ffc7a8506c57a4a
Joshua Dano <jgd5nm@virginia.edu>
2021-01-27 11:30:31 -0500

    adding new scripts based on submission

--- newsite/mysite/main/views.py
+++ newsite/mysite/main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import invasive
+import requests
+from subprocess import run, PIPE
+import sys
+import os 
+from calculate import *
 
 # Create your views here.
 def index(response):
@@ -9,17 +13,54 @@ def index(response):
 def all(response):
 	return render(response, "main/all.html", {})
 
-def form(response):
-	if response.method == "POST":
-		form = invasive(response.POST)
+def results(response):
+	return render(response, "main/results.html", {})
 
-		if form.is_valid():
-			n = form.cleaned_data["name"]
+# def output(request):
+# 	age = request.POST.get("age")
+# 	out = run(sys.executable, ['//HemoPheno//HemoPheno4HF//newsite//mysite//main//HemoPheno4HF.py', age], shell=False, stout=PIPE)
 
-		return HttpResponse(n)
+# 	print(out)
 
-	else: 
-		form = invasive()
+# 	return render(request,'main/base.html',{'data1':out})
 
-	form = invasive()
-	return render(response, "main/form.html", {"form":form})
+# def output(request):
+# 	data = requests.get("https://reqres.in/api/users")
+# 	print(data.text)
+# 	data = data.text
+# 	return render(request,"main/base.html",{'data':data})
+
+def external(request):
+	# input = {}
+	# input['age'] = request.POST.get('age')
+	# input['ejf'] = request.POST.get('ef')
+	# input['hr'] = request.POST.get('hr')
+	# input['dia'] = request.POST.get('dia')
+	# input['sys'] = request.POST.get('sys')
+	# input['rap'] = request.POST.get('rap')
+	# input['pas'] = request.POST.get('pas')
+	# input['pad'] = request.POST.get('pad')
+	# input['pamn'] = request.POST.get('pamn')
+	# input['pcwp'] = request.POST.get('pcwp')
+	# input['pcwpmn'] = request.POST.get('pcwp_mn')
+	# input['co'] = request.POST.get('co')
+	# input['ci'] = request.POST.get('ci')
+	# input['mixed'] = request.POST.get('mixed')
+	# input['mpap'] = request.POST.get('mpap')
+	# input['cpi'] = request.POST.get('cpi')
+	# input['pp'] = request.POST.get('pp')
+	# input['ppp'] = request.POST.get('ppp')
+	# input['papp'] = request.POST.get('papp')
+	# input['svr'] = request.POST.get('svr')
+	# input['rat'] = request.POST.get('rat')
+	# input['ppratio'] = request.POST.get('pprat')
+	# input['papi'] = request.POST.get('papi')
+	# input['sapi'] = request.POST.get('sapi')
+	# input['cpp'] = request.POST.get('cpp')
+	# input['praprat'] = request.POST.get('praprat')
+	# input['testparam'] = request.POST.get('testparam')
+	results = run([sys.executable, '/Users/jdano/Documents/HemoPheno/HemoPheno4HF/newsite/mysite/main/calculate.py'], shell=False, stdout=PIPE)
+	# print(results)
+	# return render(request, "main/results.html", {"score":results,"path":path})
+	return render(request, "main/results.html", {"score":results})
+
